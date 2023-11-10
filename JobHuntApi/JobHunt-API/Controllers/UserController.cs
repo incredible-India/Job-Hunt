884302627d3dd5ba10c2614b2f7a21e82bdf2889
namya <namyahs004@gmail.com>
2023-11-10 23:38:32 +0530

    Added New user model and controller

---
+++ JobHuntApi/JobHunt-API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using JobHunt_Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JonHunt_API.Controllers
+{
+    [Route("jobhunt/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        #region Post
+        //For the New User 
+        [HttpPost("registration")]
+        public OkObjectResult NewRegistration([FromBody]NewUser newUser)
+        {
+            return Ok(newUser);
+        }
+
+        #endregion
+    }
+}
---
+++ JobHuntApi/JobHunt-Models/User/NewUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunt_Models.User
+{
+    public class NewUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNo { get; set; }
+        public string AlternatePhoneNo { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
