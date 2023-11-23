e371e08db4332fc8ae5a58f2598d60539f8b7162
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-11-23 23:43:34 +0530

    UI Registration done

--- JobHuntUi/JobHunt-UI/src/app/Modules/user/user.module.ts
+++ JobHuntUi/JobHunt-UI/src/app/Modules/user/user.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
+import { NavbarComponent } from 'src/app/Components/navbar/navbar.component';
+import { UsersRoutingModule } from './user-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    RegistrationComponent
+    RegistrationComponent,
+    NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UsersRoutingModule
   ]
 })
 export class UserModule { }
--- JobHuntUi/JobHunt-UI/src/app/app.module.ts
+++ JobHuntUi/JobHunt-UI/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './Modules/user/registration/registration.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
+//import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent,
-    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
