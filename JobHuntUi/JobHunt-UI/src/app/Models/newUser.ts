2c2a9b0d71dae8849c2cf7e36bffaa47d1d8a612
namya <namyahs004@gmail.com>
2023-11-28 23:14:30 +0530

    added user registration service

---
+++ JobHuntUi/JobHunt-UI/src/app/Models/newUser.ts
@@ -0,0 +1,9 @@
+export class NewUser{
+    FirstName: string|undefined;
+    LastName:string|undefined;
+    Age: number|undefined;
+    PhoneNo: string|undefined;
+    AlternatePhoneNo: string|undefined;
+    Password: string|undefined;
+    Gender: string|undefined;
+}
--- JobHuntUi/JobHunt-UI/src/app/Modules/user/registration/registration.component.ts
+++ JobHuntUi/JobHunt-UI/src/app/Modules/user/registration/registration.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {userClient} from  "./../../../services/user-client";
+import { NewUser } from 'src/app/Models/newUser';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  /**
+   *
+   */
+  constructor(private userService : userClient) {
+      
+  }
+  ngOnInit(): void {
+    this.user();
+  }
+
+  body:NewUser = {
+    Age :12,
+    FirstName :"LastName",
+    LastName :"Namya H.S",
+     AlternatePhoneNo : "0010010111",
+     Gender : "Male",
+     Password : "1234567890",
+     PhoneNo : "1001001001",
+  }
+
+ user(){
+let  res= this.userService.UserRegistration(this.body)
+  console.log(res);
+ }
+
+ 
+  
 
 }
