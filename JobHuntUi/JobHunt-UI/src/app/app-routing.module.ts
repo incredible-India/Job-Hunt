fd4b0967ba7151f40d3ade8b475b1156233b82bb
namya <namyahs004@gmail.com>
2023-12-18 23:19:25 +0530

    created new home page

--- JobHuntUi/JobHunt-UI/src/app/app-routing.module.ts
+++ JobHuntUi/JobHunt-UI/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './Components/about/about.component';
 
 const routes: Routes = [
   {path: '',    loadChildren: () => import('./Modules/user/user.module').then(m => m.UserModule)},
+  {path:"home",loadChildren:()=>import('./Modules/home/home.module').then(m=>m.HomeModule)},
   {path: 'about', component:AboutComponent}
 ];
 
