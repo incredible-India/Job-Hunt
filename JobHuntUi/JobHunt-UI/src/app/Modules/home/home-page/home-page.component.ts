6bf4ca1e2b632859c988d4cc0a05e6179cd604cb
namya <namyahs004@gmail.com>
2023-12-23 23:25:32 +0530

    home page ui updated

--- JobHuntUi/JobHunt-UI/src/app/Modules/home/home-page/home-page.component.ts
+++ JobHuntUi/JobHunt-UI/src/app/Modules/home/home-page/home-page.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  darkMode(){
+    console.log("be happy namya");
+    
+    document.body.classList.toggle('dark')
+    if(document.body.classList[0] == "dark")
+    document.getElementsByClassName('darkmode')[0].innerHTML = "<i class=\"fa-solid fa-sun\" style=\"color:white;\"></i>"
+    else
+    document.getElementsByClassName('darkmode')[0].innerHTML = ` <i class="fa-solid fa-moon" ></i>`
+  }
+
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  navigateToDashboard() {
+    // Add code to navigate to the dashboard
+    alert('Navigating to Dashboard');
+  }
+
+  logout() {
+    // Add code to perform logout
+    alert('Logging out');
+  }
+
 }
